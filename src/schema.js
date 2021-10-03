6678a777c0b14c434d61ecf05938220d8a081ad4
Sergey Manzhak <manzhak.ru@gmail.com>
2021-10-04 00:21:19 +0300

    add favorited By

--- src/schema.js
+++ src/schema.js
@@ -11,6 +11,11 @@ const typeDefs = gql`
     createdAt: DateTime!
     updatedAt: DateTime!
   }
+  type NoteFeed {
+    notes: [Note]!
+    cursor: String!
+    hasNextPage: Boolean!
+  }
   type User {
     id: ID!
     username: String!
@@ -25,6 +30,7 @@ const typeDefs = gql`
     user(username: String!): User
     users: [User!]!
     me: User!
+    noteFeed(cursor: String): NoteFeed
   }
   type Mutation {
     toggleFavorite(id: ID!): Note!
