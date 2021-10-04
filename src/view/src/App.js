e3c6ce108636a33e6cf7d78e9b5e3a858a5e5065
Manzhak <manzhak.com@gmail.com>
2021-10-04 17:05:37 +0300

    edit npm file

--- src/view/src/App.js
+++ src/view/src/App.js
@@ -2,22 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Adam';
+  const now = String(new Date());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Hello {name}</p>
+      <p>The current time is {now}</p>
+      <p>Two plus two is {2 + 2}</p>
     </div>
   );
 }
